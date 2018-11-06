9af948d6d94ea4f3f43a9073d6f7fc667ccfa663
Josepa <yogesh.josepa393@gmail.com>
2018-11-05 20:02:15 -0600

    Inicio de sesion listo :heavy_check_mark:

--- Codigo/ProyectoVuelaSA/ProyectoVuelaSA/Controllers/HomeController.cs
+++ Codigo/ProyectoVuelaSA/ProyectoVuelaSA/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoVuelaSA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,69 @@ namespace ProyectoVuelaSA.Controllers
 {
     public class HomeController : Controller
     {
+        private ProyectoRequeEntities db = new ProyectoRequeEntities();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //ViewBag.idfederacion = new SelectList(db.federacion, "idfederacion", "idfederacion");
 
+        [HttpPost]
+        public ActionResult Index(usuario usuarioLogIn){
+
+            usuario usuario = db.usuario.Where(u=> u.idusuario == usuarioLogIn.idusuario && u.contrasena == usuarioLogIn.contrasena).FirstOrDefault();
+            if(usuario != null)
+            {
+                Session["IDUsuario"] = usuario.idusuario;
+                if(usuario.pasaporte != null)
+                {
+                    return RedirectToAction("Cliente");
+                }
+                else
+                {
+                    return RedirectToAction("Administrador");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Usuario no valido");
+            }
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Cliente()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            if(Session["IDUsuario"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
         }
+
+        public ActionResult Administrador()
+        {
+            if (Session["IDUsuario"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
     }
 }
+
+/*<div class="form-group">
+            @Html.LabelFor(model => model.idfederacion, "idfederacion", htmlAttributes: new { @class = "control-label col-md-2" })
+            <div class="col-md-10">
+                @Html.DropDownList("idfederacion", null, htmlAttributes: new { @class = "form-control", style = "height: 50px;" })
+                @Html.ValidationMessageFor(model => model.idfederacion, "", new { @class = "text-danger" })
+            </div>
+        </div>*/
